68a075fabf775db59c6a4322e34e318a0ce919cc
Riddled Ocean <ashton.wamsley@bsu.edu>
2025-09-06 16:36:18 -0400

    Added while loop and menu for user inputs

--- CS222_Assignment3.py
+++ CS222_Assignment3.py
@@ -4,11 +4,30 @@ def studentInfoTool():
     try:
         with open('students.txt', 'r') as file:
             for line in file:
-                parts = line.strip().split(' , ')
+                parts = line.strip().split(',')
                 if len(parts) == 5:
                     studentId, lastName, firstName, major, gpa = parts
                     students[studentId] = [lastName, firstName, major, gpa]
     except FileNotFoundError:
         print("Error File Not Found")
+        return
+    
+    print("Choose an option:\n1) Search by last name\n2) Search by major\n3) Quit\n")
+    loop = True
+    while loop == True:    
+        userChoice = input("Enter: ")
+        if userChoice == "1":
+            userSearch = input("Enter last name to search for: ")
+            print(students[userSearch])
+        elif userChoice == "2":
+            userSearch = input("Enter major to search for: ")
+            print(students[major])
+        elif userChoice == "3":
+            print("Goodbye!")
+            loop = False
+        else:
+            print("Invalid input try again")
+
+studentInfoTool()
     
     
