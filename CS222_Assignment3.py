e67568d580aa8a63a4a6e154186a8d09d7d2440c
Riddled Ocean <ashton.wamsley@bsu.edu>
2025-09-09 10:25:05 -0400

    Fixed the search and fixed the loop not breaking if input is 3

--- CS222_Assignment3.py
+++ CS222_Assignment3.py
@@ -18,13 +18,28 @@ def studentInfoTool():
         userChoice = input("Enter: ")
         if userChoice == "1":
             userSearch = input("Enter last name to search for: ")
-            print(students[userSearch])
+            searchFound = False
+            for studentId, data in students.items():
+                if data[0] == userSearch:
+                    print(f"{studentId}, {data[0]}, {data[1]}, {data[2]}, {data[3]}")
+                    searchFound = True
+            if not searchFound:
+                print(f"No student found with the last name of {userSearch}")
+
         elif userChoice == "2":
             userSearch = input("Enter major to search for: ")
-            print(students[major])
+            searchFound = False
+            for studentId, data in students.items():
+                if data[2] == userSearch:
+                    print(f"{studentId}, {data[0]}, {data[1]}, {data[2]}, {data[3]}")
+                    searchFound = True
+            if not searchFound:
+                print(f"No students found with the major of {userSearch}")
+        
         elif userChoice == "3":
             print("Goodbye!")
             loop = False
+        
         else:
             print("Invalid input try again")
 
